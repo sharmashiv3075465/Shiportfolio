442fa74de3d65058c799b2720d46d1c0d70b2e49
Shivam Sharma <150908320+sharmashiv3075465@users.noreply.github.com>
2023-12-28 22:01:01 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,234 @@
+export const Bio = {
+  name: "Shivam Sharma",
+  roles: [
+    "Front-End Developer ",
+    "Software Engineer",
+    "Programmer",
+  ],
+  description:
+    "A goal-oriented Entry Level Frontend Developer versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/sharmashiv3075465",
+  resume:
+    "https://drive.google.com/file/d/1YIMQQZC4PEBYV7-Y972NUAdiMdt2FZLz/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/shivam-sharma-107b4716b/",
+  twitter: "https://twitter.com/s_shivam98349",
+  insta: "https://www.instagram.com/shivamsphotogiri/",
+  facebook: "https://www.facebook.com/profile.php?id=100057354582847",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/2048px-Python.svg.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png",
+      }
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  { id: 0,
+    img: "https://cdn-images.himalayas.app/5uzzzkokpkbovsy5tqbjbikg9h8i",
+    role: "Associate",
+    company: "Tech Mahindra",
+    date: "July 2022 - Oct 2023",
+    desc: "Determining the structure and design of web pages, building reusable codes, optimizing page loading times, and using a variety of markup languages to create the web pages.",
+    skills: [
+      "CSS",
+      "HTML",
+      "React.js",
+      "redux",
+      "JavaScript",
+      "TypeScript",
+      " Next Js",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://pbs.twimg.com/profile_images/1237312667699654657/JTTG4jIA_400x400.jpg",
+    role: "Technical Sales Engineer",
+    company: "Skill-Lync",
+    date: "Dec-2021 - July-2022",
+    desc: "Provide pre- and post-sales technical advice and support about products. Employers typically prefer graduates with qualifications in computer science, software engineering, physics or maths.",
+    skills: [
+      "ReactJS",
+      "Sales",
+      "Software Development Life Cycle (SDLC)",
+      "Customer Relationship Management (CRM)",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+];
+
+export const education = [
+  { id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/3/3a/Lovely_Professional_University_logo.png",
+    school: "Lovely Professional Univeristy, Punjab",
+    date: "Aug 2017 - Sep 2021",
+    grade: "6 CGPA",
+    desc: "This comprehensive program has equipped me with a solid foundation in mechanical engineering, electrical systems, and computer science, providing me with the expertise to excel in the dynamic and evolving field of automobile engineering. This interdisciplinary program has not only honed my skills in mechanical and electrical engineering but has also provided me with a solid foundation in computer science, enhancing my ability to navigate the cutting-edge technologies shaping the Every field of Technology",
+    degree: "Bachelor of Technology - BTech, Automobile Engineering",
+  },
+  {
+    id: 1,
+    img: "https://vectorseek.com/wp-content/uploads/2023/08/Nios-Logo-Vector.svg-.png",
+    school: "H.S.S.E (Intermediate)",
+    date: "Apr 2016 - Apr 2017",
+    grade: "81.2%",
+    desc: "I completed my class 12 high school education at National Istitute of Open Schooling, where I studied Science with With Physic, Chemisty and Maths.",
+    degree: "NIOS(XII), Science with Maths",
+  },
+  {
+    id: 2,
+    img: "https://play-lh.googleusercontent.com/bOUpfsxjAZih2CfHCt_PGznJGW6J4EaUxy6obY8VHW3R3dA6kTNlbvxTCAvpOpH-xg=w600-h300-pc0xffffff-pd",
+    school: "Glenhill School, Varanasi",
+    date: "Apr 2013 - Apr 2014",
+    grade: "72.3%",
+    desc: "I completed my class 10 education at Glenhill School, Varanasi.",
+    degree: "CBSC(X), General Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Movix",
+    date: "Nov 2023 - Dec 2023",
+    description:
+      "This Movie App Project is a cutting-edge application crafted with JavaScript, React, and Redux, offering users and engaging cinematic experience. It provides a user-friendly interface for users, this project showcases the synergy of JavaScript, React and Redux, and Redux in delivering a feature-rich and responsive movie-watching platform.", 
+    image: "https://scontent.fdel11-2.fna.fbcdn.net/v/t39.30808-6/414662314_887016566553488_8846011310674407323_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=3635dc&_nc_ohc=PGQZwfjOvD4AX9X23Sl&_nc_ht=scontent.fdel11-2.fna&oh=00_AfAsqiMKnUflruLu20DicIDx-g1LOn-_XQJu2XptxeioHQ&oe=65922536",
+    tags: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "JSON",
+      "SCSS",
+      "React Js",
+      "Redux",
+    ],
+    category: "Web app",
+    github: "https://github.com/sharmashiv3075465/newProject",
+    webapp: "https://new-project-blond-six.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Amazon-Clone",
+    date: "Nov 2023 - Nov 2023",
+    description:
+      "The Amazon Clone Webpage project is a meticulously crafted e-commerce platform developed solely with JavaScript",
+    image:
+      "https://scontent.flko9-2.fna.fbcdn.net/v/t39.30808-6/414980585_887017836553361_4801682348336690441_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3635dc&_nc_ohc=aMMN_diWH5MAX-Lt1BN&_nc_ht=scontent.flko9-2.fna&oh=00_AfASou1hIh5ZZcE4hNoOgIlQ-4u5RxpnXUxwC4K4UMNdQA&oe=659075EE",
+    tags: ["HTML", "CSS", "JavaScript", "Flex"],
+    category: "Web app",
+    github: "https://github.com/sharmashiv3075465/Amazon-clone-webpage",
+    webapp: "https://sharmashiv3075465.github.io/Amazon-clone-webpage/",
+  },
+  {
+    id: 1,
+    title: "YouTube-Clone",
+    date: "Aug 2023 - Aug 2023",
+    description:
+      "This project is all about to recreated the YouTube front page using just HTML and CSS. This project reflects the best use of HTML and CSS.",
+    image:
+      "https://scontent.flko9-2.fna.fbcdn.net/v/t39.30808-6/414819826_887030529885425_8214649516834622111_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=VAvwCRDjxrEAX-urR33&_nc_ht=scontent.flko9-2.fna&oh=00_AfB-OX88_kQi1BM-Rr82hn7rmRFWO47Y6u8mPGZid7Dj1A&oe=659120A7",
+    tags: [
+      "HTML",
+      "CSS",
+    ],
+    category: "Web app",
+    github: "https://github.com/sharmashiv3075465/Shivam-Youtube.clone-HTML-CSS",
+    webapp: "https://shivam-youtube-clone-html-css.vercel.app/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
